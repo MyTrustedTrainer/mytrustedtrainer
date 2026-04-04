4768a6c6912511144da1f77d32b39503b4f50da8
MyTrustedTrainer <support@mytrustedtrainer.com>
2026-04-04 11:11:37 -0700

    edit/page: add showBack to SiteHeader

--- app/dashboard/trainer/edit/page.tsx
+++ app/dashboard/trainer/edit/page.tsx
@@ -118,7 +118,7 @@ export default function TrainerEditProfile() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <SiteHeader />
+      <SiteHeader showBack />
 
 
       <div className="max-w-4xl mx-auto px-4 py-8">
